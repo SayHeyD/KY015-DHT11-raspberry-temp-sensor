76cd4c6c96e50e26275ab1b155336d783be4d4cd
David Docampo <david@opsone.ch>
2024-12-06 23:58:38 +0100

    add API route

--- web-app/routes/api.php
+++ web-app/routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\TemperatureEntryController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::middleware('auth:sanctum')
+    ->prefix('/v1')
+    ->name('api.v1.')
+    ->group(function () {
+        Route::prefix('/temperatures')->name('temperatures.')->group(function () {
+            Route::post('/store', [TemperatureEntryController::class, 'store'])->name('store');
+        });
+});
