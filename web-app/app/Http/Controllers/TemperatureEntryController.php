abc756b01863ede8fddf58df91f6e47dd4241b63
David Docampo <david@opsone.ch>
2024-12-06 23:58:32 +0100

    add logic for storing TemperatureEntries

--- web-app/app/Http/Controllers/TemperatureEntryController.php
+++ web-app/app/Http/Controllers/TemperatureEntryController.php
@@ -2,9 +2,33 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\StoreTemperatureEntryRequest;
+use App\Models\Device;
+use App\Models\TemperatureEntry;
+use Illuminate\Support\Facades\Auth;
 
 class TemperatureEntryController extends Controller
 {
-    //
+    public function store(StoreTemperatureEntryRequest $request)
+    {
+        $device = Auth::user();
+
+        if (! $device instanceof Device::class) {
+            return response()
+                ->json(
+                    [
+                        'message' => 'Temperature entries must be created by devices.'
+                    ],
+                    403
+                );
+        }
+
+        $tempEntry = TemperatureEntry::create([
+            'device_id' => $device->id,
+            'temperature' => $request->temperature,
+            'humidity' => $request->humidity,
+        ]);
+
+        return response()->json(['id' => $tempEntry->id],201);
+    }
 }
