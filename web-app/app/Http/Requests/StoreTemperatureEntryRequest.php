afccf07b20584a9db6fa2eba9c2f04d07f6f6bb4
David Docampo <david@opsone.ch>
2024-12-06 23:40:56 +0100

    define StoreTemperatureEntryRequest.php

---
+++ web-app/app/Http/Requests/StoreTemperatureEntryRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+
+class StoreTemperatureEntryRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return Auth::check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'temperature' => ['required', 'numeric', 'min:0', 'max:9000'],
+            'humidity' => ['required', 'numeric', 'min:0', 'max:100'],
+        ];
+    }
+}
