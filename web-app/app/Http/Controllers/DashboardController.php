05f17e5f8684f42c6229a565c5b180f1332586bc
David Docampo <david@docampo.ch>
2024-12-15 03:03:50 +0100

    load device with temperature entries from the last 24h, newest entries first

--- web-app/app/Http/Controllers/DashboardController.php
+++ web-app/app/Http/Controllers/DashboardController.php
@@ -9,8 +9,17 @@ class DashboardController extends Controller
 {
     public function view()
     {
+        $devices = Auth::user()->devices;
+
         return Inertia::render('Dashboard/View', [
-            'devices' => Auth::user()->devices,
+            // 'devices' => [],
+            'devices' => $devices->load([
+                'temperatures' => function($query) {
+                    $query
+                        ->where('created_by', '>=', now()->subDay())
+                        ->orderBy('created_at', 'desc');
+                }
+            ]),
         ]);
     }
 }
