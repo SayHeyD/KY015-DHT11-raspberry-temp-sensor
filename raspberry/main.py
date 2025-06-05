44dd7362b37520eca155e4eb8b1a1f7f075e99f1
David Docampo <david@docampo.ch>
2025-06-05 10:29:09 +0200

    Update GPIO pin Number

--- raspberry/main.py
+++ raspberry/main.py
@@ -2,7 +2,7 @@ import time
 import board
 import adafruit_dht
 # Initialize the DHT with the data pin connected to pin 16 (GPIO 23) of the Raspberry Pi:
-dhtDevice = adafruit_dht.DHT11(board.D23)
+dhtDevice = adafruit_dht.DHT22(board.D4)
 # You can pass DHT22 use_pulseio=False if you do not want to use pulseio.
 # This may be necessary on a Linux single board computer like the Raspberry Pi, but it will not work in CircuitPython.
 # dhtDevice = adafruit_dht.DHT22(board.D18, use_pulseio=False)
