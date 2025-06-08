e1a499f9bbcbb6e1638aa5bedfd1398d353a2c41
David Docampo <david@docampo.ch>
2025-06-08 15:08:30 +0200

    ignore sync error if stderr is a cli

--- agent/main.go
+++ agent/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"errors"
 	"github.com/SayHeyD/raspberry-temp-sensor/agent/cmd"
 	"github.com/SayHeyD/raspberry-temp-sensor/agent/internal/app"
 	"go.uber.org/zap"
 	"log"
+	"syscall"
 )
 
 func main() {
@@ -18,7 +20,8 @@ func main() {
 	sugar := logger.Sugar()
 	defer func(logger *zap.Logger) {
 		err := logger.Sync()
-		if err != nil {
+		// Ignore sync errors if stderr is a cli
+		if err != nil && !errors.Is(err, syscall.ENOTTY) {
 			sugar.Fatal(err)
 		}
 	}(logger)
