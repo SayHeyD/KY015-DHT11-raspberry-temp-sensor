5cb6a974f2a0ba7d11871f0ced6cbaa3fb5f20df
David Docampo <david@docampo.ch>
2025-06-09 03:00:17 +0200

    switch back to minimal python after extensive testing

--- agent/pkg/gpio/gpioChipInfo.go
+++
@@ -1,9 +0,0 @@
-package gpio
-
-// C-style struct for ioctl calls, translated to Go
-type GPIOChipInfo struct {
-	Name  [32]byte
-	Label [32]byte
-	Lines uint32
-	// Go will handle padding if any, ensure C struct alignment if issues arise
-}
--- agent/pkg/gpio/gpioHandleData.go
+++
@@ -1,6 +0,0 @@
-package gpio
-
-// C-style struct for ioctl calls, translated to Go
-type GPIOHandleData struct {
-	Values [GPIOHANDLES_MAX]uint8 // Values to set or get
-}
