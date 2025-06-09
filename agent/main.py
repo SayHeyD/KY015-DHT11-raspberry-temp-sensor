11ed51424410fccf5210a8b1b25b4de21353c1ad
David Docampo <david@docampo.ch>
2025-06-09 17:30:11 +0200

    add env variable config to python script

--- agent/main.py
+++ agent/main.py
@@ -1,34 +1,64 @@
-# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
-# SPDX-License-Identifier: MIT
-
 import time
-
+import os
 import board
+import json
 
 import adafruit_dht
 
-# Initial the dht device, with data pin connected to:
-dhtDevice = adafruit_dht.DHT22(board.D4)
+import requests
 
-# you can pass DHT22 use_pulseio=False if you wouldn't like to use pulseio.
-# This may be necessary on a Linux single board computer like the Raspberry Pi,
-# but it will not work in CircuitPython.
-# dhtDevice = adafruit_dht.DHT22(board.D18, use_pulseio=False)
+## Environment variables
+
+# Connection information
+SERVER_HOST = os.environ['RTSA_SERVER_HOST']
+API_TOKEN = os.environ['RTSA_API_TOKEN']
+
+# Device information
+GPIO_PIN_NUMBER = os.environ['RTSA_GPIO_PIN_NUMBER']
+DEVICE_ID = os.environ['RTSA_DEVICE_ID']
+
+# Env variable validation and default values
+if SERVER_HOST is None:
+    raise Exception("RTSA_SERVER_HOST environment variable must be set. Example: http://192.168.1.164:8443")
+
+if API_TOKEN is None:
+    raise Exception("RTSA_API_TOKEN environment variable must be set.")
+
+if GPIO_PIN_NUMBER is None:
+    GPIO_PIN_NUMBER = 4
+
+if DEVICE_ID is None:
+    raise Exception("RTSA_DEVICE_ID environment variable must be set. The device ID can be fetched from the webapp UI")
+
+# Initialize DHT sensor connection
+dhtDevice = adafruit_dht.DHT22(board["D" + GPIO_PIN_NUMBER])
+
+# HTTP Request headers
+headers = {
+    'content-type': 'application/json',
+    'Authorization': 'Bearer ' + API_TOKEN
+}
 
 while True:
     try:
         # Print the values to the serial port
         temperature_c = dhtDevice.temperature
         humidity = dhtDevice.humidity
-        print(f"{temperature_c:.1f} C    Humidity: {humidity}% ")
+        print(f"{temperature_c:.1f} C | Humidity: {humidity}% ")
+
+        requests.post(SERVER_HOST + "/api/v1/temperatures", headers=headers, data=json.dumps({
+            "device_id": DEVICE_ID,
+            "temperature": temperature_c,
+            "humidity": humidity,
+        }))
 
     except RuntimeError as error:
         # Errors happen fairly often, DHT's are hard to read, just keep going
         print(error.args[0])
-        time.sleep(2.0)
+        time.sleep(0.5)
         continue
     except Exception as error:
         dhtDevice.exit()
         raise error
 
-    time.sleep(2.0)
+    time.sleep(59.0)
