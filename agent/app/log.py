453511918510ea9cbf2a5ebbc0cec80b5eb32fa6
David Docampo <david@docampo.ch>
2025-06-15 22:59:50 +0200

    update debug log color to blue

--- agent/app/log.py
+++ agent/app/log.py
@@ -19,11 +19,12 @@ class CustomFormatter(logging.Formatter):
     yellow = "\x1b[33;20m"
     red = "\x1b[31;20m"
     bold_red = "\x1b[31;1m"
+    blue = "\x1b[34;20m"
     reset = "\x1b[0m"
     format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s (%(filename)s:%(lineno)d)"
 
     FORMATS = {
-        logging.DEBUG: grey + format + reset,
+        logging.DEBUG: blue + format + reset,
         logging.INFO: grey + format + reset,
         logging.WARNING: yellow + format + reset,
         logging.ERROR: red + format + reset,
