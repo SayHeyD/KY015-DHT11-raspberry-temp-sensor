dd087b1b0b7d04153559a87954e6ec2fa0c8a054
David Docampo <david@opsone.ch>
2025-06-16 09:02:21 +0200

    add information if sensor data is mocked

--- agent/app/sensor/sensor_data.py
+++ agent/app/sensor/sensor_data.py
@@ -1,15 +1,18 @@
 from datetime import datetime
-from numbers import Number
+
+from app import app
 
 
 class SensorData:
     __temperature: float = 0
     __humidity: float = 0
+    __mock: bool = True
     __timestamp: datetime
 
     def __init__(self, temperature: float, humidity: float):
         self.__temperature = temperature
         self.__humidity = humidity
+        self.__mock = app.is_mock()
         self.__timestamp = datetime.now()
 
     def get_temperature(self):
@@ -18,8 +21,14 @@ class SensorData:
     def get_humidity(self):
         return self.__humidity
 
+    def get_mock(self):
+        return self.__mock
+
     def get_timestamp(self):
         return self.__timestamp
 
     def __str__(self):
-        return "SensorData[temperature: " + str(self.__temperature) + ", humidity: " + str(self.__humidity) + ", timestamp: " + str(self.__timestamp) + "]"
+        return ("SensorData[temperature: " + str(self.__temperature) +
+                ", humidity: " + str(self.__humidity) +
+                ", mock: " + str(self.__mock) +
+                ", timestamp: " + str(self.__timestamp) + "]")
