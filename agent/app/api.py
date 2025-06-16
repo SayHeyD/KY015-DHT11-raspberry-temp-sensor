301016d4d3e689bd2e2d4eb1a4a7858a3c2bd4d4
David Docampo <david@opsone.ch>
2025-06-16 10:04:44 +0200

    improve error handling und logging

--- agent/app/api.py
+++ agent/app/api.py
@@ -23,15 +23,18 @@ class Api:
         }
 
         # Debug logging
-        debug_message = "HTTP Headers:\n"
-        for name, value in headers:
-            debug_message += f"\t{name}: {value}\n"
-
-        app.get_logger().debug(debug_message)
+        app.get_logger().debug("HTTP Headers:")
+        for name, value in headers.items():
+            app.get_logger().debug(f"\t{name}: {value}")
 
         return headers
 
     def send_data(self, sensor_data: SensorData):
+        api_endpoint = self.__server_host + self.__temperature_route
+
+        app.get_logger().debug(f"Transmitting data to: '{api_endpoint}' ...")
+
+        occurred_error = None
 
         try:
             request_body = json.dumps({
@@ -42,12 +45,23 @@ class Api:
             })
 
             app.get_logger().debug(
-                f"Transmitting measurement data:\n\n{str(request_body)}"
+                f"Transmitting measurement data:{request_body}"
             )
-            response = requests.post(self.__server_host + self.__temperature_route,
+            response = requests.post(api_endpoint,
                           headers=self.__get_headers(),
                           data=request_body
             )
-            app.get_logger().info(f"Transmitted measurement successfully: HTTP {response.status_code}")
+            if response.status_code != 201:
+                app.get_logger().error(f"Did not receive HTTP 201, instead HTTP {response.status_code} was received.")
+            else:
+                app.get_logger().info(f"Transmitted measurement successfully: HTTP {response.status_code}")
+
+            app.get_logger().debug(f"Response headers: {response.headers.items()}")
+            app.get_logger().debug(f"Response body: {response.text.replace("\n", "\\n")}")
+        except requests.ConnectionError as error:
+            occurred_error = f"Could not connect to host '{self.__server_host}': {error.args}"
         except requests.HTTPError as error:
-            app.get_logger().error(f"Failed to transmit measurement data: HTTP {error.response.status}: {error.response}")
+            occurred_error = f"HTTP {error.response.status}: {error.args}"
+        finally:
+            if occurred_error is not None:
+                app.get_logger().error(f"Failed to transmit measurement data: {occurred_error})")
