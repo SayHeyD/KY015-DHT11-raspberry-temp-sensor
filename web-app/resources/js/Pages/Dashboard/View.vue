ae71b15a86e334154d1b1123dffd130b6566cd70
David Docampo <david@opsone.ch>
2025-06-16 10:10:13 +0200

    fix dropdown overflow styling

--- web-app/resources/js/Pages/Dashboard/View.vue
+++ web-app/resources/js/Pages/Dashboard/View.vue
@@ -17,7 +17,7 @@ defineProps({
 
     <div class="py-12">
       <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-        <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
+        <div class="bg-white dark:bg-gray-800 shadow-xl sm:rounded-lg">
           <DeviceData :devices="devices" />
         </div>
       </div>
