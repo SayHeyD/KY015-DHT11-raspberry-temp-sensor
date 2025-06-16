3cdd8a61c5074f7ab7b1a19ed71c4dcabc3f513c
David Docampo <david@opsone.ch>
2025-06-16 08:34:11 +0200

    allow config based log level

--- agent/main.py
+++ agent/main.py
@@ -1,12 +1,22 @@
 import logging
+import os
 import time
 
 from app import log
 from app import app
 from app.sensor import create_sensor
 
+__log__level = logging.INFO
+
+# set log level
+try:
+    level = os.environ['RTSA_LOG_LEVEL']
+    __log__level = logging.getLevelNamesMapping()[level]
+except KeyError:
+    pass
+
 # Setup logger
-logger = log.setup("rtsa")
+logger = log.setup("rtsa", __log__level)
 app.configure()
 
 sensor = create_sensor.create()
