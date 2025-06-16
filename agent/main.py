44196d71f3f3f10375b643d1fa598306a9f91ce2
David Docampo <david@opsone.ch>
2025-06-16 09:32:40 +0200

    fix log_level config handling

--- agent/main.py
+++ agent/main.py
@@ -5,18 +5,39 @@ import time
 from app import log
 from app import app
 from app.sensor import create_sensor
+from app.api import Api
 
-__log__level = logging.INFO
+# Loglevel settings
+__requested_level = None
+__log__level = None
+__log_level_invalid = False
 
-# set log level
+# Set log level
 try:
-    level = os.environ['RTSA_LOG_LEVEL']
-    __log__level = logging.getLevelNamesMapping()[level]
-except KeyError:
-    pass
+    __requested_level = os.environ['RTSA_LOG_LEVEL']
 
-# Setup logger
+    # Check if a valid log level was passed
+    try:
+         __log__level = logging.getLevelNamesMapping()[__requested_level]
+    except KeyError:
+        __log_level_invalid = __log__level is None
+
+    if __log_level_invalid:
+        __log__level = logging.INFO
+
+# If the environment variable is not set, default to "INFO"
+except KeyError as error:
+    __log__level = logging.INFO
+
+# Setup logger and configure app
 logger = log.setup("rtsa", __log__level)
+logger.info(f"Loglevel set to: '{logging.getLevelName(__log__level)}'")
+
+if __log_level_invalid:
+    valid_log_levels = "'DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'"
+    logger.critical(f"Invalid log level '{__requested_level}'. Valid values are {valid_log_levels}.")
+    exit(1)
+
 app.configure()
 
 sensor = create_sensor.create()
